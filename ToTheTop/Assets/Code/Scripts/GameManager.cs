c093001d36434c53a6d9868032d75f6b99c49c2a
sebasrvallejo <sebasx23@hotmail.es>
2023-11-07 23:38:10 -0500

    Dialogue system
    Dialogue System in Level Design

--- ToTheTop/Assets/Code/Scripts/GameManager.cs
+++ ToTheTop/Assets/Code/Scripts/GameManager.cs
@@ -28,13 +28,7 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-        }
-
         Instance = this;
-        DontDestroyOnLoad(gameObject);
     }
 
     private void Start()
